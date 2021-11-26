d34f6c992917dbb976471cdb94089a98f72b5daf
trshpnd <brcts990@gmail.com>
2021-11-26 14:07:00 -0300

    alpha update

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0x8c;
+static const unsigned int alpha = 0xff;
 static const unsigned int field_alpha = 0xff;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
